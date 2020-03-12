753acc2bfe8f8160461b854580ff96bd3476d39c
Eduardo <egarci135@gmail.com>
2020-03-12 14:52:59 -0700

    React

---
+++ src/main/resources/static/precache-manifest.2aa6ea607d9479633478cab3503144f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265a6cde526634a038b1094061aed615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb863505569bf3d0882c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "596c14c9a4a5ee12ff21",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "cb863505569bf3d0882c",
+    "url": "/static/js/2.4aee3ce8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.4aee3ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596c14c9a4a5ee12ff21",
+    "url": "/static/js/main.3780d54c.chunk.js"
+  },
+  {
+    "revision": "11c9d864873bb505fc06",
+    "url": "/static/js/runtime-main.c170e6a8.js"
+  }
+]);
